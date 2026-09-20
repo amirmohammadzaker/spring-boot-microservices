31033ca28c3e4c63b0fb957c4686a86ffe2d0170
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-09-20 14:50:46 +0330

    feat: add AccountsServiceImpl skeleton with repository dependencies

--- accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
@@ -1,4 +1,4 @@
-package com.eazybytes.accounts.Repository;
+package com.eazybytes.accounts.repository;
 
 import com.eazybytes.accounts.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
