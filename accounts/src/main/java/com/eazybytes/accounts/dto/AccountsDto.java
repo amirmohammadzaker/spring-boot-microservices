c43b28e107e526769349e99b0e395c7ab5963d7a
Amir Mohammad Zaker <amzzaaker@gmail.com>
2026-09-21 17:53:01 +0330

    feat: add field validation annotations to AccountsDto fields

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,14 +1,22 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType can not be a null or empty")
+    @Size(min = 3, max = 30, message = "AccountType length should be between 3 and 30") // در صورت نیاز
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
-
 }
